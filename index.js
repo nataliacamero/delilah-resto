858f475611dd4671ad929acc5ff83ddc44f51c28
John Doe <nataliacameroc@gmail.com>
2020-05-10 20:48:44 -0500

    Midelware auenticarUsuario en los endpoints

--- index.js
+++ index.js
@@ -73,9 +73,9 @@ sequelize.sync({ force: true }).then(() => {
 
     Usuarios.bulkCreate([
       { nombre: 'Natalia', apellido: "Camero", email:"nataliacameroc@gmail.com", telefono: "3013606833", direccioEnvio:"Calle 145 # 46 78", nombreUsuario: "nataliacameroc@gmail.com", password: "KJUBHYAS&&%TUGYGYJ", rol: "Administrador" },
-      { nombre: 'Camilo', apellido: "Barrera", email:"camilobarrera@gmail.com", telefono: "3023445566", direccioEnvio:"Calle 1 # 56 99", nombreUsuario: "camilobarrera@gmail.com", password: "jg76ertwygbdsfy", rol: "usuario" },
-      { nombre: 'Andres', apellido: "Ayala", email:"andresayala@gmail.com", telefono: "3036775477", direccioEnvio:"Carrera 45 # 8 67", nombreUsuario: "andresayala@gmail.com", password: "srfnw7ryiwhfjksdh", rol: "usuario" },
-      { nombre: 'Gustavo', apellido: "Maggi", email:"gustavomaggi@gmail.com", telefono: "3057634387", direccioEnvio:"Calle 145 # 46 78", nombreUsuario: "gustavomaggi@gmail.com", password: "jhgf77tqwyxnfjd", rol: "usuario" },
+      { nombre: 'Camilo', apellido: "Barrera", email:"camilobarrera@gmail.com", telefono: "3023445566", direccioEnvio:"Calle 1 # 56 99", nombreUsuario: "camilobarrera@gmail.com", password: "jg76ertwygbdsfy", rol: "usuarioBasico" },
+      { nombre: 'Andres', apellido: "Ayala", email:"andresayala@gmail.com", telefono: "3036775477", direccioEnvio:"Carrera 45 # 8 67", nombreUsuario: "andresayala@gmail.com", password: "srfnw7ryiwhfjksdh", rol: "usuarioBasico" },
+      { nombre: 'Gustavo', apellido: "Maggi", email:"gustavomaggi@gmail.com", telefono: "3057634387", direccioEnvio:"Calle 145 # 46 78", nombreUsuario: "gustavomaggi@gmail.com", password: "jhgf77tqwyxnfjd", rol: "usuarioBasico" },
     ]).then(function() {
       
       return Usuarios.findAll();
@@ -95,6 +95,72 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Delilah Restó App'));
 
 
+//Login
+
+app.post('/login', (req, res) => {
+
+  let {nombreUsuario, password} = req.body;
+  console.log("Linea 103", nombreUsuario);
+  console.log("Linea 104", password);
+
+  sequelize.authenticate().then(async () => {
+    let {nombreUsuario, password} = req.body;
+    const query = `SELECT nombre,nombreUsuario,password,rol FROM Delilah_Resto.usuarios WHERE nombreUsuario = ${JSON.stringify(nombreUsuario)} AND password = ${JSON.stringify(password)};`;
+    const [resultados] =  await sequelize.query(query, { raw: true })
+    console.log(resultados);
+    if (resultados.length > 0) {
+      const token = jwt.sign({nombre: resultados[0].nombre, password: password, rol: resultados[0].rol}, firma);
+      res.json(token);
+      console.log(token)
+      console.log("Linea 115: ", resultados[0].nombre);
+      console.log("Linea 116: ", resultados[0].password);
+      console.log("Linea 117: ", resultados[0].rol);
+      return
+    } else {
+      res.json({ error: "Usuario o contraseña incorrecta."});
+      return
+    }
+  })
+});
+
+
+//Metodo Seguro
+
+const autenticarUsuario = (req, res, next) => {
+console.log("Linea 130: ",req.headers.authorization);
+console.log("Linea 131: ",req.headers.authorization.split(' ')[1]);
+try{
+  const token = req.headers.authorization.split(' ')[1];
+  console.log(token);
+  const verificarToken = jwt.verify(token, firma);
+  if(verificarToken){
+    req.usuario = verificarToken;
+    return next();
+  }
+} catch(error) {
+  res.json({ error: "Error al validar el usuario."})
+}
+};
+
+app.post('/seguro', autenticarUsuario, (req,res) => {
+res.send(`Esta es una página autenticada. Hola ${req.usuario.nombre}. Tu rol es ${req.usuario.rol}`)
+});
+
+
+// Verificar rol
+function authRole(role) {
+
+ return (req, res, next) => {
+   if (req.usuario.rol !== role) {
+     res.status(401)
+     return res.send('Not allowed')
+  
+   next()
+ }
+
+};
+
+
 
 
 
@@ -105,7 +171,7 @@ app.get('/', (req, res) => res.send('Delilah Restó App'));
 
 //Get Usuarios
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios', autenticarUsuario, (req, res) => {
     
   sequelize.authenticate().then(async () => {
         
@@ -125,7 +191,7 @@ app.get('/usuarios', (req, res) => {
 
 //Get USUARIO por id
 
-app.get('/usuarios/:indiceUsuarios', (req, res) => {
+app.get('/usuarios/:indiceUsuarios', autenticarUsuario, (req, res) => {
  
   sequelize.authenticate().then(async () => {
     const indiceUsuarios = req.params.indiceUsuarios;
@@ -143,7 +209,7 @@ app.get('/usuarios/:indiceUsuarios', (req, res) => {
 
 //Post, opcion de crear una nueva cuenta USUARIO
 
-app.post('/usuarios', function(req, res) {
+app.post('/usuarios', autenticarUsuario, function(req, res) {
   Usuarios.create({ nombre: req.body.nombre, apellido: req.body.apellido, email: req.body.email, telefono: req.body.telefono , direccioEnvio: req.body.direccioEnvio, nombreUsuario: req.body.nombreUsuario, password: req.body.password, rol: req.body.rol }).then(function(nombre) {
     res.json(nombre);
     console.log("Usuario creado")
@@ -152,7 +218,7 @@ app.post('/usuarios', function(req, res) {
 
 //Put, actualizar USUARIO por id
 
-app.put('/usuarios/:indiceUsuario', function(req, res) {
+app.put('/usuarios/:indiceUsuario', autenticarUsuario, function(req, res) {
 
   const indiceUsuario = req.params.indiceUsuario;
   Usuarios.findByPk(req.params.indiceUsuario).then(function(nombre) {
@@ -173,7 +239,7 @@ app.put('/usuarios/:indiceUsuario', function(req, res) {
 
 //Delete, borrar USUARIO por id
 
-app.delete('/usuarios/:indiceUsuario', function(req, res) {
+app.delete('/usuarios/:indiceUsuario', autenticarUsuario, function(req, res) {
   const indiceUsuario = req.params.indiceUsuario;
   Usuarios.findByPk(req.params.indiceUsuario).then(function(nombre) {
     nombre.destroy();
@@ -186,7 +252,7 @@ app.delete('/usuarios/:indiceUsuario', function(req, res) {
 
 //Get productos
 
-app.get('/productos', (req, res) => {
+app.get('/productos', autenticarUsuario, (req, res) => {
 
   sequelize.authenticate().then(async () => {
 
@@ -205,7 +271,7 @@ app.get('/productos', (req, res) => {
 
 //Get productos por id
 
-app.get('/productos/:indiceProductos', (req, res) => {
+app.get('/productos/:indiceProductos', autenticarUsuario, (req, res) => {
  
   sequelize.authenticate().then(async () => {
     const indiceProductos = req.params.indiceProductos;
@@ -223,7 +289,7 @@ app.get('/productos/:indiceProductos', (req, res) => {
 
 //Post, crear productos
 
-app.post('/productos', function(req, res) {
+app.post('/productos', autenticarUsuario, function(req, res) {
   Productos.create({ nombre: req.body.nombre, imagen: req.body.imagen, precio: req.body.precio }).then(function(nombre) {
     res.json(nombre);
   });
@@ -231,7 +297,7 @@ app.post('/productos', function(req, res) {
 
 //Put, actualizar producto por id
 
-app.put('/productos/:indiceProductos', function(req, res) {
+app.put('/productos/:indiceProductos', autenticarUsuario, function(req, res) {
 
   const indiceProductos = req.params.indiceProductos;
   Productos.findByPk(req.params.indiceProductos).then(function(nombre) {
@@ -247,7 +313,7 @@ app.put('/productos/:indiceProductos', function(req, res) {
 
 //Delete, borrar producto por id
 
-app.delete('/productos/:indiceProductos', function(req, res) {
+app.delete('/productos/:indiceProductos', autenticarUsuario, function(req, res) {
   const indiceProductos = req.params.indiceProductos;
   Productos.findByPk(req.params.indiceProductos).then(function(nombre) {
     nombre.destroy();
@@ -256,56 +322,4 @@ app.delete('/productos/:indiceProductos', function(req, res) {
   });
 });
 //------------------------------------------------------------------------------------------------------------------
-
-
-
-//Login
-
-app.post('/login', (req, res) => {
-
-  let {nombreUsuario, password} = req.body;
-  console.log("Linea 266", nombreUsuario);
-  console.log("Linea 267", password);
-
-  sequelize.authenticate().then(async () => {
-    let {nombreUsuario, password} = req.body;
-    const query = `SELECT nombre, nombreUsuario,password FROM Delilah_Resto.usuarios WHERE nombreUsuario = ${JSON.stringify(nombreUsuario)} AND password = ${JSON.stringify(password)};`;
-    const [resultados] =  await sequelize.query(query, { raw: true })
-    console.log(resultados);
-    if (resultados.length > 0) {
-      const token = jwt.sign({nombre: resultados[0].nombre, password: password}, firma);
-      res.json(token);
-      console.log(token)
-      console.log("Linea 278: ", resultados[0].nombreUsuario);
-      console.log("Linea 279: ", resultados[0].password);
-      return
-    } else {
-      res.json({ error: "Usuario o contraseña incorrecta."});
-      return
-    }
-  })
-});
-
-
-//Metodo Seguro
-
-const autenticarUsuario = (req, res, next) => {
-console.log(req.headers.authorization);
-console.log(req.headers.authorization.split(' ')[1]);
-try{
-  const token = req.headers.authorization.split(' ')[1];
-  console.log(token);
-  const verificarToken = jwt.verify(token, firma);
-  if(verificarToken){
-    req.usuario = verificarToken;
-    return next();
-  }
-} catch(error) {
-  res.json({ error: "Error al validar el usuario."})
-}
-};
-
-app.post('/seguro', autenticarUsuario, (req,res) => {
-res.send(`Esta es una página autenticada. Hola ${req.usuario.nombre}`)
-});
 
